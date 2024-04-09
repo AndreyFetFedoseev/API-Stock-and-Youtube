14afb14b153fe150ee21f68185d3660c64156c65
Andrey Fedoseev <MaxviP100@yandex.ru>
2024-04-09 15:08:25 +0700

    API stock_price_checker

--- stock_price_checker/main.py
+++ stock_price_checker/main.py
@@ -7,26 +7,27 @@ import requests
 API_KEY = 'cnr8fnpr01qs2jr5mj30cnr8fnpr01qs2jr5mj3g'
 BASE_URL = 'https://finnhub.io/api/v1/'
 DAYS_BACK = 7
-SYMBOL = "TSLA"
+SYMBOL = "YNDX"
 
 
 def check_price():
-    url = f"{BASE_URL}quote?symbol={SYMBOL}&token={API_KEY}"
+    stock = input('Введите абревиатуру акции: ')
+    url = f"{BASE_URL}quote?symbol={stock}&token={API_KEY}"
     response = requests.get(url)
     data = json.loads(response.text)
-    print(data)
+    print(f'{stock} = {data['c']} $')
 
-    params = {
-        'symbol': SYMBOL,
-        'resolution': 'D',
-        'from': int(datetime.timestamp(datetime.now() - timedelta(days=DAYS_BACK))),
-        'to': int(datetime.timestamp(datetime.now())),
-        'token': API_KEY
-    }
-    url = f"{BASE_URL}stock/candle"
-    response = requests.get(url, params=params)
-    data_candles = json.loads(response.text)
-    print(data_candles)
+    # params = {
+    #     'symbol': SYMBOL,
+    #     'resolution': 'D',
+    #     'from': int(datetime.timestamp(datetime.now() - timedelta(days=DAYS_BACK))),
+    #     'to': int(datetime.timestamp(datetime.now())),
+    #     'token': API_KEY
+    # }
+    # url = f"{BASE_URL}stock/candle"
+    # response = requests.get(url, params=params)
+    # data_candles = json.loads(response.text)
+    # print(data_candles)
     # last_days_back_average = sum(data_candles['c']) / len(data_candles['c'])
 
     # if data['c'] > last_days_back_average:
